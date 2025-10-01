25bc7c35642b33ec146c449144a1d894297c53f0
sdneal517 <sdneal517@gmail.com>
2025-10-01 12:17:33 -0400

    syncing home PC state to GitHub

--- run_refresh.cmd
+++ run_refresh.cmd
@@ -1,12 +1,9 @@
 @echo off
-echo === Starting run_refresh.cmd === >> src\logs\cmd_trace.log
-echo %DATE% %TIME% >> src\logs\cmd_trace.log
-
-REM Navigate to project root
 cd /d C:\Users\sdnea\Dev\mlb-data-pipeline
-echo Changed directory to %CD% >> src\logs\cmd_trace.log
 
-REM Run the pipeline with venv python explicitly
-C:\Users\sdnea\Dev\mlb-data-pipeline\venv\Scripts\python.exe src\refresh_all.py >> src\logs\cmd_trace.log 2>&1
+echo === Starting run_refresh.cmd === >> logs\cmd_trace.log
+echo %DATE% %TIME% >> logs\cmd_trace.log
 
-echo === Finished run_refresh.cmd === >> src\logs\cmd_trace.log
+C:\Users\sdnea\Dev\mlb-data-pipeline\venv\Scripts\python.exe src\refresh_all.py >> logs\cmd_trace.log 2>&1
+
+echo === Finished run_refresh.cmd === >> logs\cmd_trace.log
